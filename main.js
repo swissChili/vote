8c7d7570db7d9c64ae8dcab1643f0f1fa9c1f4b8
Desmond <#>
2017-09-28 17:46:48 -0700

    SPELLED

--- main.js
+++ main.js
@@ -4,13 +4,8 @@ var app = new Vue({
     modal: false,
     modalCandidate: {},
     canidates: [{
-<<<<<<< HEAD
       name: 'Leam Jenney',
       motto: 'Take a free drink',
-=======
-      name: 'Liam Jenney',
-      moto: 'Take a free drink',
->>>>>>> 725f0fdb934a1b9906e6356fe702cc043838e9c6
       values: 'Eat food, sleep well, play, meh',
       image: './images/profile_placeholder.png'
     }, {
@@ -20,7 +15,7 @@ var app = new Vue({
       image: './images/profile_placeholder.png'
     }, {
       name: 'Leo Fayad',
-      moto: 'A face you can trust.',
+      motto: 'A face you can trust.',
       values: 'None, Fame, Lazars!',
       image: './images/profile_placeholder.png'
     }]
