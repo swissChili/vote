21af0a6d09e9f83f549d61b1f2ac3cc0c8102ed9
swissChili <sasha@fastmail.com>
2018-02-08 11:51:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-  var socket = io('localhost:8000')
   var app = new Vue({
     el: '#app',
     data: {
@@ -10,49 +9,7 @@
       modalCandidate: {},
       canidates: []
     },
-    mounted: function () {
-      firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-          var name = user.displayName
-          var email = user.email
-          var photoUrl = user.photoURL
-          var emailVerified = user.emailVerified
-          var uid = user.uid
-          socket.emit('checkIfVoted', uid)
-          socket.on('allreadyvoted', function () {
-            window.location.href = '/voted.html'
-          })
-          socket.on('checksOut', function (canidates) {
-            app.loginState = true
-            socket.emit('getCanidates')
-            socket.on('canidates', function (canidates) {
-              console.log(app.canidates)
-              console.log('got canidates', canidates)
-              app.canidates = canidates
-            })
-          })
-          app.loginMessage = 'Logout'
-        } else {
-          app.loginMessage = 'Login'
-          app.loginState = false
-        }
-      })
-    },
     methods: {
-      login: function () {
-        console.log('loging in')
-        var user = firebase.auth().currentUser
-        if (user) {
-          firebase.auth().signOut().then(function () {
-            app.loginMessage = 'Login'
-            app.loginState = false
-          }).catch(function (error) {
-          // An error happened.
-          })
-        } else {
-          firebase.auth().signInWithRedirect(provider)
-        }
-      },
       openModal: function (canidate) {
         this.modalCandidate = canidate
         this.modal = true
@@ -60,27 +17,5 @@
       closeModal: function () {
         this.modal = false
       },
-      voteFor: function (canidate) {
-        var user = firebase.auth().currentUser
-        var name, email, photoUrl, uid, emailVerified
-
-        if (user != null) {
-          name = user.displayName
-          email = user.email
-          photoUrl = user.photoURL
-          emailVerified = user.emailVerified
-          uid = user.uid  // The user's ID, unique to the Firebase project. Do NOT use
-                   // this value to authenticate with your backend server, if
-                   // you have one. Use User.getToken() instead.
-        }
-        console.log('voted for', canidate.name)
-        socket.emit('vote', canidate.name, uid, email, name)
-        this.modal = false
-        this.voted = true
-        var stateObj = {
-          foo: 'bar'
-        }
-        history.pushState(stateObj, 'Voted ', 'voted.html')
-      }
     }
   })
